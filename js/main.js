78577315377b4ef6454f9970bb2fa09a6ef1184d
kiana <soltani@shadzi.club>
2021-09-18 10:43:43 +0430

    javascript-form

--- js/main.js
+++ js/main.js
@@ -1,54 +1,11 @@
-// $(".carousel_se_01_carousel").owlCarousel({
-//     autoplay:true,
-//     autoplayhoverpause:true,
-//     autoplaytimeout:100,
-//     items:3,
-//     nav: true,
-//     loop: true,
-//     lazyLoad:true,
-//     margin: 5,
-//     padding: 5,
-//     stagePadding: 5,
-//     responsive: {
-//         0: {
-//             items: 1,
-//             dots: false
-//         },
-//         485: {
-//             items: 2,
-//             dots: false
-//         },
-//         728: {
-//             items:3,
-//             dots: false
-//         },
-//         960: {
-//             items: 3,
-//             dots: false
-//         },
-//         1200: {
-//             items: 3,
-//             dots: true
-//         }
-//     }
-// });
-
-
-/*Require flicky lib 
-https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js
-*/
-
-
-
-   $(document).ready(function () {
-    $(".carousel_se_01_carousel").owlCarousel({
-    autoplay:true,
-    autoplayhoverpause:true,
-    autoplaytimeout:100,
-    items:3,
-    // nav: true,
+$(".carousel_se_01_carousel").owlCarousel({
+    autoplay: true,
+    autoplayhoverpause: true,
+    autoplaytimeout: 100,
+    items: 3,
+    nav: true,
     loop: true,
-    lazyLoad:true,
+    lazyLoad: true,
     margin: 5,
     padding: 5,
     stagePadding: 5,
@@ -62,7 +19,7 @@ https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js
             dots: false
         },
         728: {
-            items:3,
+            items: 3,
             dots: false
         },
         960: {
@@ -74,45 +31,99 @@ https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js
             dots: true
         }
     }
-    });
-  });
+});
 
-  $(document).ready(function () {
-    $(".carousel_se_02_carousel").owlCarousel({
-      // autoplay:true,
-      // autoplayhoverpause:true,
-      // autoplaytimeout:100,
-        items:1,
-        video:true,
-        lazyLoad:true,
-        center:true,
-        responsive:{
-            480:{
-                items:1
+
+/*Require flicky lib 
+https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js
+*/
+
+$(document).ready(function () {
+    $(".carousel_se_01_carousel").owlCarousel({
+        autoplay: true,
+        autoplayhoverpause: true,
+        autoplaytimeout: 100,
+        items: 3,
+        // nav: true,
+        loop: true,
+        lazyLoad: true,
+        margin: 5,
+        padding: 5,
+        stagePadding: 5,
+        responsive: {
+            0: {
+                items: 1,
+                dots: false
             },
-            600:{
-                items:1
+            485: {
+                items: 2,
+                dots: false
+            },
+            728: {
+                items: 3,
+                dots: false
+            },
+            960: {
+                items: 3,
+                dots: false
+            },
+            1200: {
+                items: 3,
+                dots: true
             }
         }
     });
-  });
+});
 
+$(document).ready(function () {
+    $(".carousel_se_02_carousel").owlCarousel({
+        // autoplay:true,
+        // autoplayhoverpause:true,
+        // autoplaytimeout:100,
+        items: 1,
+        video: true,
+        lazyLoad: true,
+        center: true,
+        responsive: {
+            480: {
+                items: 1
+            },
+            600: {
+                items: 1
+            }
+        }
+    });
+});
 
+// myform.addEventListener('submit',function(e){
+// document.location.href=""
+// })
 
-//   function setCookie( name, value , exdays) {
-//     var mydate ,expires , user;
-//     user = document.getElementById("user").value;
-//     mydate = new Date();
-//     mydate.setTime(mydate.getTime() + (exdays*24*60*60*1000));
-//     expires = "expires="+ mydate.toUTCString();
-//     document.cookie = name + "=" + value + ";" + expires + ";path=/";
-//     if( value == "" ){
-//       alert("بی زحمت یه چیزی وارد کن");
-//     }else{
-//       setCookie("user", value , 365);
-//     }
-//   }
+const destinationUrl = "https://www.skyroom.online/ch/soroush1993/functional";
 
+function handleForm(e) {
+    e.preventDefault();
+    var form_data = new FormData(document.getElementById("form"));
 
-//     fetch("https://testapi.shadzi.club/campaigns/digikalla/user/submit",{
+    fetch('https://testapi.shadzi.club/campaigns/digikalla/user/submit', {
+        method: 'POST',
+        body: form_data,
+    }).then(function (response) {
+        localStorage.setItem("fullname", document.getElementById('fullname').value);
+        localStorage.setItem("mobileNumber", document.getElementById('mobileNumber').value);
+        window.location.assign(destinationUrl);
+    }).catch(function (error) {
+        console.error(error);
+    });
 
+}
+
+function onDocLoad() {
+    if (fullname = localStorage.getItem("fullname")) {
+        document.getElementById("fullname").value = fullname;
+    }
+    if (mobileNumber = localStorage.getItem("mobileNumber")) {
+        document.getElementById("mobileNumber").value = mobileNumber;
+    }
+    return true;
+}
